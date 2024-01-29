2333e590b5e8ee84915ea51e3a7e691577c4588a
SDRS <sdrs.1234@gmx.de>
2024-01-29 19:32:15 +0100

    Initial commit

---
+++ src/main/java/de/sdrs/servermanager_v2/ServerManager.java
@@ -0,0 +1,17 @@
+package de.sdrs.servermanager_v2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
