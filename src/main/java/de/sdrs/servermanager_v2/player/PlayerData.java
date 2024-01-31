3bd71d40f3016b1aac6dbd191a99e59d7dddec8e
SDRS <sdrs.1234@gmx.de>
2024-01-31 17:37:26 +0100

    0.0.0.2

--- src/main/java/de/sdrs/servermanager_v2/player/PlayerData.java
+++ src/main/java/de/sdrs/servermanager_v2/player/PlayerData.java
@@ -5,38 +5,61 @@ import de.sdrs.servermanager_v2.plugin.main.ServerManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class PlayerData implements PlayerActions {
 
-    private HashMap<Object, Object> data = new HashMap<>();
-    private HashMap<Object, Object> playerData = new HashMap<>();
+    private static HashMap<Object, Object> data = new HashMap<>();
+    private static HashMap<Object, Object> playerData = new HashMap<>();
+    private String playerID;
 
     public PlayerData(Player player) {
         SMAPI smapi = new SMAPI();
         HashMap<Object, Object> tmpData = smapi.readFromYAML(ServerManager.getDir() + "/players.yml");
-        if (tmpData == null || !tmpData.containsKey(player.getUniqueId().toString())) {
+        playerID = player.getUniqueId().toString();
+        if (tmpData == null || !tmpData.containsKey(playerID)) {
             createPlayer(player);
         } else {
-            playerData = (HashMap<Object, Object>) tmpData.get(player.getUniqueId().toString());
+            playerData = (HashMap<Object, Object>) tmpData.get(playerID);
         }
     }
 
     private void createPlayer(Player player) {
         HashMap<Object, Object> pData = new HashMap<>();
+        HashMap<Object,Object> location = new HashMap<>();
+
+        location.put("world", player.getWorld().getName());
+        location.put("x", player.getLocation().getX());
+        location.put("y", player.getLocation().getY());
+        location.put("z", player.getLocation().getZ());
+        location.put("yaw", player.getLocation().getYaw());
+        location.put("pitch", player.getLocation().getPitch());
 
         pData.put("name", player.getName());
-        pData.put("location", player.getLocation());
+        pData.put("location", location);
 
-        data.put(player.getUniqueId().toString(), pData);
+        data.put(playerID, pData);
 
         savePlayerData();
     }
 
     public void add(Object key,Object value) {
         playerData.put(key, value);
-        data.replace(Bukkit.getPlayer((String) playerData.get("name")).getUniqueId().toString(), playerData);
+        data.replace(playerID, playerData);
         savePlayerData();
+    }
+
+    public static List<String> get() {
+        List<String> tmp = new ArrayList<>();
+        for (Object key : data.keySet()) {
+            tmp.add((String) data.get(key));
+        }
+        for (Object key : data.keySet()) {
+            tmp.add((String) playerData.get(key));
+        }
+        return tmp;
     }
     @Override
     public void savePlayerData() {
@@ -47,7 +70,7 @@ public class PlayerData implements PlayerActions {
     @Override
     public void saveLocation() {
         playerData.replace("location", Bukkit.getPlayer((String) playerData.get("name")).getLocation());
-        data.replace(Bukkit.getPlayer((String) playerData.get("name")).getUniqueId().toString(), playerData);
+        data.replace(playerID, playerData);
         savePlayerData();
     }
 }
--- src/main/java/de/sdrs/servermanager_v2/plugin/main/testCMD.java
+++ src/main/java/de/sdrs/servermanager_v2/plugin/main/testCMD.java
@@ -1,15 +1,25 @@
 package de.sdrs.servermanager_v2.plugin.main;
 
 import de.sdrs.servermanager_v2.player.PlayerData;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class testCMD implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         PlayerData playerData = new PlayerData((Player) sender);
-        return false;
+        Bukkit.getConsoleSender().sendMessage(playerData.toString());
+        List data = PlayerData.get();
+        for (Object i : data) {
+            Bukkit.getConsoleSender().sendMessage((String) i);
+        }
+        return true;
     }
 }
