8a7c5af68e60a351286bab618740b6db8856b957
SDRS <87233933+TheSDRS@users.noreply.github.com>
2024-02-15 00:13:52 +0100

    Delete SpigotServer directory

--- SpigotServer/ServerStartWithPlugin.bat
+++
@@ -1,2 +0,0 @@
-xcopy "C:\Users\David\IdeaProjects\ServerManager_v2\target\ServerManager_v2-1.0-SNAPSHOT.jar" C:\Users\David\IdeaProjects\ServerManager_v2\SpigotServer\plugins /Y
-java -Xmx1G -jar server.jar nogui
