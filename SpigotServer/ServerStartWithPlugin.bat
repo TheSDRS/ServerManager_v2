c09ff60e81feec3d7b7caf85f8bf748269ee5d08
SDRS <sdrs.1234@gmx.de>
2024-02-15 00:12:59 +0100

    0.0.0.3

---
+++ SpigotServer/ServerStartWithPlugin.bat
@@ -0,0 +1,2 @@
+xcopy "C:\Users\David\IdeaProjects\ServerManager_v2\target\ServerManager_v2-1.0-SNAPSHOT.jar" C:\Users\David\IdeaProjects\ServerManager_v2\SpigotServer\plugins /Y
+java -Xmx1G -jar server.jar nogui
